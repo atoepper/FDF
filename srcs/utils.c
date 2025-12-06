99b4221490ba4856545e21c4fcc79ea0b0cae92d
atoepper <atoepper@student.42wolfsburg.de>
2025-12-06 10:46:06 +0100

    add function print_map

--- srcs/utils.c
+++ srcs/utils.c
@@ -34,3 +34,14 @@ int	arr_size(char **arr)
 		i++;
 	return (i);
 }
+
+void	print_map(t_map *map)
+{
+	if (map->max_z && map->min_z)
+		ft_printf("min-z: %d, max_z: %d\n", map->min_z, map->max_z);
+	if (map->max.x && map->max.y)
+		ft_printf("max.x: %d, max.y: %d\n", map->max.x, map->max.y);
+	printf("ZOOM: x: %f, y: %f, z: %f\n", map->zoom.x, map->zoom.y, map->zoom.z);
+	printf("ROTATION: x: %f, y: %f, z: %f\n", map->rot.x, map->rot.y, map->rot.z);
+	printf("OFFSET: x: %f, y: %f, z: %f\n", map->offset.x, map->offset.y, map->offset.z);
+}
