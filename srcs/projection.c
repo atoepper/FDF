75963b31bac2bbffa657e35724d1c98499c92a0f
atoepper <atoepper@student.42wolfsburg.de>
2025-12-08 15:07:10 +0100

    add  map projection

--- srcs/projection.c
+++ srcs/projection.c
@@ -6,47 +6,70 @@
 /*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 13:53:58 by atoepper          #+#    #+#             */
-/*   Updated: 2025/10/30 16:52:13 by atoepper         ###   ########.fr       */
+/*   Updated: 2025/12/08 14:37:45 by atoepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/fdf.h"
 
-// t_vec	rot(t_vec vec, t_vec rot, t_vec offset, t_vec zoom)
-// {
-// 	t_vec	p;
-// 	t_vec	temp;
+static void		translate(t_map *map, t_index i)
+{
+	t_vec	offset;
 
-// 	temp.x = zoom.x * vec.x;
-// 	temp.y = zoom.x * vec.y;
-// 	p.x = (cos(rot.z) * temp.x - sin(rot.z) * temp.y);
-// 	p.y = (sin(rot.z) * temp.x + cos(rot.z) * temp.y);
-// 	p.z = vec.z * zoom.z;
-// 	temp = p;
-// 	p.x = cos(rot.y) * temp.x + sin(rot.y) * temp.z;
-// 	p.z = -sin(rot.y) * temp.x + cos(rot.y) * temp.z;
-// 	temp = p;
-// 	p.y = cos(rot.x) * temp.y - sin(rot.x) * temp.z;
-// 	p.z = sin(rot.x) * temp.y + cos(rot.x) * temp.z;
-// 	p.x += offset.x;
-// 	p.y += offset.y;
-// 	p.color = vec.color;
-// 	return (p);
-// }
+	offset = map->offset;
+	map->proj[i.y][i.x].x += offset.x;
+	map->proj[i.y][i.x].y += offset.y;
+	map->proj[i.y][i.x].z += offset.z;
+}
 
-// int	transformation(t_map *map)
-// {
-// 	t_index	i;
+static void		rotate(t_map *map, t_index i)
+{
+	t_vec	rot;
+	t_vec	temp;
 
-// 	i.y = -1;
-// 	while (++i.y < map->max.y)
-// 	{
-// 		i.x = -1;
-// 		while (++i.x < map->max.x)
-// 		{
-// 			map->proj[i.y][i.x] = rot(map->transf[i.y][i.x],
-// 					map->rot, map->center, map->zoom);
-// 		}
-// 	}
-// 	return (0);
-// }
+	rot = map->rot;
+	temp = map->proj[i.y][i.x];
+	map->proj[i.y][i.x].x = (cos(rot.z) * temp.x - sin(rot.z) * temp.y);
+	map->proj[i.y][i.x].y = (sin(rot.z) * temp.x + cos(rot.z) * temp.y);
+	temp = map->proj[i.y][i.x];
+	map->proj[i.y][i.x].x = cos(rot.y) * temp.x + sin(rot.y) * temp.z;
+	map->proj[i.y][i.x].z = -sin(rot.y) * temp.x + cos(rot.y) * temp.z;
+	temp = map->proj[i.y][i.x];
+	map->proj[i.y][i.x].y = cos(rot.x) * temp.y - sin(rot.x) * temp.z;
+	map->proj[i.y][i.x].z = sin(rot.x) * temp.y + cos(rot.x) * temp.z;
+}
+
+static void		zoom(t_map *map, t_index i)
+{
+	map->proj[i.y][i.x].x *= map->zoom.x;
+	map->proj[i.y][i.x].y *= map->zoom.y;
+	map->proj[i.y][i.x].z *= map->zoom.z;
+}
+
+static void		map_to_cartesian(t_map *map, t_index i)
+{
+	map->proj[i.y][i.x].x = ((double)i.x
+			- (((double)map->max.x - 1) / 2));
+	map->proj[i.y][i.x].y = ((double)i.y
+			- (((double)map->max.y - 1) / 2));
+	map->proj[i.y][i.x].z = map->h[i.y][i.x];
+}
+
+int	project_map(t_map *map)
+{
+	t_index	i;
+
+	i.y = -1;
+	while (++i.y < map->max.y)
+	{
+		i.x = -1;
+		while (++i.x < map->max.x)
+		{
+			map_to_cartesian(map, i);
+			zoom(map, i);
+			rotate(map, i);
+			translate(map, i);
+		}
+	}
+	return (0);
+}
