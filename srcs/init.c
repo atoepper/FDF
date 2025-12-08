8d6959e5377f0b0d1ffe56cf31364360311b2cd3
atoepper <atoepper@student.42wolfsburg.de>
2025-12-08 15:09:50 +0100

    restructure function init_map

--- srcs/init.c
+++ srcs/init.c
@@ -6,91 +6,73 @@
 /*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:39:19 by atoepper          #+#    #+#             */
-/*   Updated: 2025/10/30 14:07:27 by atoepper         ###   ########.fr       */
+/*   Updated: 2025/12/08 14:51:56 by atoepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/fdf.h"
 
-void	set_default(t_map *map)
+static void	import_colors(t_map *map)
+{
+	t_index	i;
+
+	while (++i.y < map->max.y)
+	{
+		i.x = -1;
+		while (++i.x < map->max.x)
+			map->proj[i.y][i.x].z = map->col[i.y][i.x];
+	}	
+}
+
+static void	calculate_default_zoom(t_map *map)
+{
+	// h = rot_rect_h(PI / 4, map->max.x, map->max.y);
+	/* calculate zoom factor */
+	
+	// map->zoom.x = (HEIGHT - 50) / h;
+	// if (map->zoom.x * hypotenuse(map->max.x, map->max.y)
+	// 	> WIDTH - hypotenuse(map->max.x, map->max.y))
+	// 	map->zoom.x = WIDTH - hypotenuse(map->max.x, map->max.y);
+	// map->zoom.z = map->zoom.x / 2;	
+}
+
+static void	set_default(t_map *map)
 {
 	double	h;
 
 	map->center.x = WIDTH / 2;
 	map->center.y = HEIGHT / 2;
 	map->center.z = 0;
+	map->offset = map->center;
 	map->rot.x = (atan(1 / sqrt(2)));
 	map->rot.y = (0);
 	map->rot.z = (PI / 4);
-	h = rot_rect_h(PI / 4, map->max.x, map->max.y);
-	map->zoom.x = (HEIGHT - 50) / h;
-	if (map->zoom.x * hypotenuse(map->max.x, map->max.y)
-		> WIDTH - hypotenuse(map->max.x, map->max.y))
-		map->zoom.x = WIDTH - hypotenuse(map->max.x, map->max.y);
-	map->zoom.z = map->zoom.x / 2;
+	calculate_default_zoom(map);
 }
 
-// int	init_map(t_map *map)
-// {
-// 	t_index	i;
-
-// 	i.y = -1;
-// 	while (++i.y < map->max.y)
-// 	{
-// 		i.x = -1;
-// 		while (++i.x < map->max.x)
-// 		{
-// 			map->transf[i.y][i.x].x = ((double)i.x
-// 					- (((double)map->max.x - 1) / 2));
-// 			map->transf[i.y][i.x].y = ((double)i.y
-// 					- (((double)map->max.y - 1) / 2));
-// 			map->transf[i.y][i.x].z = map->h[i.y][i.x];
-// 		}
-// 	}
-// 	set_default(map);
-// 	transformation(map);
-// 	return (0);
-// }
-
-long	get_colors(t_map *map, char **raw)
+static void	malloc_projection(t_map *map)
 {
-	size_t	n;
-	int		c;
+	int	y;
 
-	n = 0;
-	if (!*raw || **raw != ',')
-		return (0xaaaaaa);
-	*raw += 3;
-	while (*(*raw + n) && (ft_ishex(*(*raw + n))))
-		n++;
-	c = ft_hextoi(*raw, n);
-	*raw += n;
-	map->colored = true;
-	return (c);
+	map->proj = malloc(sizeof(t_vec *) * map->max.y);
+	if (!map->proj)
+		exit_on_error(map, "Fatal error: malloc fail");
+	y = 0;
+	while (y < map->max.y)
+	{
+		map->proj[y] = malloc(sizeof(t_vec) * map->max.x);
+		if (!map->proj[y++])
+			exit_on_error(map, "Fatal error: malloc fail");
+	}
 }
 
-// void	get_height_color(t_map *map, char *raw)
-// {
-// 	t_index	i;
+int	init_map(t_map *map)
+{
+	t_index	i;
 
-// 	map->min_z = 0;
-// 	map->max_z = 0;
-// 	i.y = -1;
-// 	while (++i.y < map->max.y)
-// 	{
-// 		i.x = -1;
-// 		while (++i.x < map->max.x)
-// 		{
-// 			while (*raw && (*raw == ' ' || *raw == '\n'))
-// 				++raw;
-// 			map->h[i.y][i.x] = ft_atoi(raw);
-// 			if (map->h[i.y][i.x] > map->max_z)
-// 				map->max_z = map->h[i.y][i.x];
-// 			if (map->h[i.y][i.x] < map->min_z)
-// 				map->min_z = map->h[i.y][i.x];
-// 			while (*raw && (ft_isdigit(*raw) || *raw == '-'))
-// 				++raw;
-// 			map->transf[i.y][i.x].color = get_colors(map, &raw);
-// 		}
-// 	}
-// }
+	malloc_projection(map);
+	set_default(map);
+	import_colors(map);
+	project_map(map);
+	return (SUCCESS);
+}
