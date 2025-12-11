53035416c836c565aaaee9cd4e8f93d5cd9b19a3
atoepper <atoepper@student.42wolfsburg.de>
2025-12-11 13:52:04 +0100

    add function update_window

--- srcs/render_map.c
+++ srcs/render_map.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 12:29:23 by atoepper          #+#    #+#             */
+/*   Updated: 2025/12/11 12:30:39 by atoepper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../incl/fdf.h"
 
-void	draw_map(t_img img, t_map *map)
+void	draw_map(t_img *img, t_map *map)
 {
 	t_index	i;
 
@@ -16,4 +28,11 @@ void	draw_map(t_img img, t_map *map)
 				draw_line(img, map->proj[i.y][i.x], map->proj[i.y][i.x + 1]);
 		}
 	}
+}
+
+void	update_window(t_map *map)
+{
+	draw_map(map->img_map, map);
+	mlx_put_image_to_window(map->mlx, map->win,
+		map->img_map->mlx_img, 0, 0);
 }
