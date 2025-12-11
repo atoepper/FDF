4422eb5287fb4205875c898817230eff4733fef0
atoepper <atoepper@student.42wolfsburg.de>
2025-12-11 13:52:51 +0100

    add print color to fct print_vector

--- srcs/vector.c
+++ srcs/vector.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 12:44:11 by atoepper          #+#    #+#             */
+/*   Updated: 2025/12/11 12:44:12 by atoepper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../incl/fdf.h"
 
 void	set_vector(t_vec *v, double x, double y, double z)
@@ -9,5 +21,5 @@ void	set_vector(t_vec *v, double x, double y, double z)
 
 void	print_vector(t_vec v)
 {
-	printf("x: %fy: %fz: %f", v.x, v.y, v.z);
+	printf("x: %fy: %fz: %f, color: %ld ", v.x, v.y, v.z, v.color);
 }
