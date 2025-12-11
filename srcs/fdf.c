5ba31b88d08652dd901c8897453c997a5a1e276b
atoepper <atoepper@student.42wolfsburg.de>
2025-12-11 13:47:45 +0100

    add functions create_image and clear_window

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,11 +6,35 @@
 /*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:49:22 by atoepper          #+#    #+#             */
-/*   Updated: 2025/12/08 13:16:22 by atoepper         ###   ########.fr       */
+/*   Updated: 2025/12/11 12:43:00 by atoepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/fdf.h"
+
+t_img	*create_image(t_map *map, int width, int height)
+{
+	t_img	*img;
+
+	img = malloc(sizeof(t_img));
+	if (img == NULL)
+		exit_on_error(map, "fatal: malloc error");
+	img->mlx_img = mlx_new_image(map->mlx, width, height);
+	if (img == NULL)
+		exit_on_error(map, "Image creation failed");
+	img->addr = mlx_get_data_addr(img->mlx_img, &img->bpp,
+			&img->line_length, &img->endian);
+	return (img);
+}
+
+void	clear_window(t_map *map)
+{
+	mlx_destroy_image(map->mlx, map->img_map->mlx_img);
+	map->img_map->mlx_img = mlx_new_image(map->mlx, WIDTH, HEIGHT);
+	map->img_map->addr = mlx_get_data_addr(map->img_map->mlx_img,
+			&map->img_map->bpp, &map->img_map->line_length,
+			&map->img_map->endian);
+}
 
 void	create_window(t_map *map)
 {
@@ -31,15 +55,20 @@ int	start_mlx(t_map *map)
 int	fdf(t_map *map)
 {
 	(void)map;
+	ft_printf("Start mlx...\n");
 	start_mlx(map);
+	ft_printf("Init map...\n");
 	init_map(map);
-	// draw_map(map->img, map);
-
-	// put_pixel(map->img, WIDTH/2, HEIGHT/2, create_trgb(0, 255, 0, 0));
-	// mlx_put_image_to_window(map->img.mlx, map->img.win, map->img.mlx_img, 0, 0);
+	map->img_map = create_image(map, WIDTH,
+			HEIGHT);
+	ft_printf("Create first view...\n");
+	draw_map(map->img_map, map);
 	create_window(map);
+	ft_printf("Render first view...\n");
+	mlx_put_image_to_window(map->mlx, map->win, map->img_map->mlx_img, 0, 0);
+	ft_printf("Set hooks...\n");
 	set_hooks(map);
+	ft_printf("Start loop...\n");
 	mlx_loop(map->mlx);
-	// cleanup(map);
 	return (0);
 }
