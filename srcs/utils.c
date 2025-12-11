414046b749726d6457fbd0a2a367f80444132736
atoepper <atoepper@student.42wolfsburg.de>
2025-12-11 13:53:28 +0100

    add print projection to fct print_map

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 12:42:21 by atoepper          #+#    #+#             */
+/*   Updated: 2025/12/11 12:44:00 by atoepper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../incl/fdf.h"
 
 void	print_2d_arr(long **arr, size_t x, size_t y)
@@ -8,7 +20,7 @@ void	print_2d_arr(long **arr, size_t x, size_t y)
 	if (!arr)
 	{
 		ft_printf("Empty Array\n");
-		return;
+		return ;
 	}
 	i = 0;
 	while (i < y)
@@ -26,8 +38,9 @@ void	print_2d_arr(long **arr, size_t x, size_t y)
 
 int	arr_size(char **arr)
 {
-	int i = 0;
+	int	i;
 
+	i = 0;
 	if (!arr)
 		return (0);
 	while (arr[i])
@@ -37,11 +50,27 @@ int	arr_size(char **arr)
 
 void	print_map(t_map *map)
 {
+	t_index	i;
+
 	if (map->max_z && map->min_z)
 		ft_printf("min-z: %d, max_z: %d\n", map->min_z, map->max_z);
 	if (map->max.x && map->max.y)
 		ft_printf("max.x: %d, max.y: %d\n", map->max.x, map->max.y);
-	printf("ZOOM: x: %f, y: %f, z: %f\n", map->zoom.x, map->zoom.y, map->zoom.z);
-	printf("ROTATION: x: %f, y: %f, z: %f\n", map->rot.x, map->rot.y, map->rot.z);
-	printf("OFFSET: x: %f, y: %f, z: %f\n", map->offset.x, map->offset.y, map->offset.z);
+	printf("ZOOM: x: %f, y: %f, z: %f\n", map->zoom.x, map->zoom.y,
+		map->zoom.z);
+	printf("ROTATION: x: %f, y: %f, z: %f\n", map->rot.x, map->rot.y,
+		map->rot.z);
+	printf("OFFSET: x: %f, y: %f, z: %f\n", map->offset.x, map->offset.y,
+		map->offset.z);
+	printf("Projection:\n");
+	i.y = -1;
+	while (++i.y < map->max.y)
+	{
+		i.x = -1;
+		while (++i.x < map->max.x)
+		{
+			print_vector(map->proj[i.y][i.x]);
+		}
+		ft_printf("\n");
+	}
 }
