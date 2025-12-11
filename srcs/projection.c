735774d26b7155255b59fb11dcaddac361e5ed5e
atoepper <atoepper@student.42wolfsburg.de>
2025-12-11 13:51:52 +0100

    correct norm

--- srcs/projection.c
+++ srcs/projection.c
@@ -6,13 +6,13 @@
 /*   By: atoepper <atoepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 13:53:58 by atoepper          #+#    #+#             */
-/*   Updated: 2025/12/08 14:37:45 by atoepper         ###   ########.fr       */
+/*   Updated: 2025/12/11 12:48:09 by atoepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/fdf.h"
 
-static void		translate(t_map *map, t_index i)
+static void	translate(t_map *map, t_index i)
 {
 	t_vec	offset;
 
@@ -22,7 +22,7 @@ static void		translate(t_map *map, t_index i)
 	map->proj[i.y][i.x].z += offset.z;
 }
 
-static void		rotate(t_map *map, t_index i)
+static void	rotate(t_map *map, t_index i)
 {
 	t_vec	rot;
 	t_vec	temp;
@@ -39,14 +39,14 @@ static void		rotate(t_map *map, t_index i)
 	map->proj[i.y][i.x].z = sin(rot.x) * temp.y + cos(rot.x) * temp.z;
 }
 
-static void		zoom(t_map *map, t_index i)
+static void	zoom(t_map *map, t_index i)
 {
 	map->proj[i.y][i.x].x *= map->zoom.x;
 	map->proj[i.y][i.x].y *= map->zoom.y;
 	map->proj[i.y][i.x].z *= map->zoom.z;
 }
 
-static void		map_to_cartesian(t_map *map, t_index i)
+static void	map_to_cartesian(t_map *map, t_index i)
 {
 	map->proj[i.y][i.x].x = ((double)i.x
 			- (((double)map->max.x - 1) / 2));
